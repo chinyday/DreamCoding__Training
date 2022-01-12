02f66528523fb1e22365aebe1222271da94bd7f3
chinyday <47979888+chinyday@users.noreply.github.com>
2022-01-12 15:53:13 +0900

    성능 개선 작업

--- coordinates/index.js
+++ coordinates/index.js
@@ -2,13 +2,13 @@ let ver = document.querySelector('.ver');
 let hor = document.querySelector('.hor');
 let target = document.querySelector('.target');
 let tag = document.querySelector('.tag');
+let targetHalWidth = target.getBoundingClientRect().width / 2; 
+let targetHalHeight = target.getBoundingClientRect().height / 2; 
 
 window.addEventListener("mousemove", (e) => {
-    ver.style.left = `${e.clientX}px`;
-    hor.style.top =  `${e.clientY}px`;
-    target.style.left = `${e.clientX}px`;
-    target.style.top = `${e.clientY}px`;
-    tag.style.left = `${e.clientX}px`;
-    tag.style.top = `${e.clientY}px`;
+    ver.style.transform = `translateX(${e.clientX}px)`;
+    hor.style.transform = `translateY(${e.clientY}px)`;
+    target.style.transform = `translate(${e.clientX-targetHalWidth}px, ${e.clientY-targetHalHeight}px)`;
+    tag.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
     tag.innerHTML = `x : ${e.clientX}px, y : ${e.clientY}px`;
 })
