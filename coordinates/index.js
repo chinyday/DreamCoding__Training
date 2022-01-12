1ecffad0c56e0472e2026a9cf273c26340b1d763
chinyday <47979888+chinyday@users.noreply.github.com>
2022-01-12 14:17:14 +0900

    마우스 커서를 따라다니는 포인터 만들기

---
+++ coordinates/index.js
@@ -0,0 +1,17 @@
+let ver = document.querySelector('.ver');
+let hor = document.querySelector('.hor');
+let target = document.querySelector('.target');
+let tag = document.querySelector('.tag');
+
+window.addEventListener("mousemove", function (e) {
+    console.log(e);
+
+    ver.style.left = `${e.clientX}px`;
+    hor.style.top =  `${e.clientY}px`;
+    target.style.left = `${e.clientX}px`;
+    target.style.top = `${e.clientY}px`;
+    tag.style.left = `${e.clientX}px`;
+    tag.style.top = `${e.clientY}px`;
+
+    tag.innerHTML = `x : ${e.clientX}px, y : ${e.clientY}px`;
+})
