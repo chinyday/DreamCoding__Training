0087d0e91ec5d4fde759ce415cf62cf642e2e0df
chinyday <47979888+chinyday@users.noreply.github.com>
2022-01-12 14:18:29 +0900

    화살표 함수로 변경 및 로그 삭제

--- coordinates/index.js
+++ coordinates/index.js
@@ -3,15 +3,12 @@ let hor = document.querySelector('.hor');
 let target = document.querySelector('.target');
 let tag = document.querySelector('.tag');
 
-window.addEventListener("mousemove", function (e) {
-    console.log(e);
-
+window.addEventListener("mousemove", (e) => {
     ver.style.left = `${e.clientX}px`;
     hor.style.top =  `${e.clientY}px`;
     target.style.left = `${e.clientX}px`;
     target.style.top = `${e.clientY}px`;
     tag.style.left = `${e.clientX}px`;
     tag.style.top = `${e.clientY}px`;
-
     tag.innerHTML = `x : ${e.clientX}px, y : ${e.clientY}px`;
 })
