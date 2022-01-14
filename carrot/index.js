6a5ff48f45d0d6fbe236d89adaa07e1517b46345
chinyday <47979888+chinyday@users.noreply.github.com>
2022-01-14 18:45:31 +0900

    타이머, 카운터, 당근 찾기 이벤트 추가

--- carrot/index.js
+++ carrot/index.js
@@ -1,26 +1,34 @@
 'use strict'
 
+const IMG_SIZE = 80;
+let IMG_COUNT = 5;
+let GAME_TIME = 20
 let playground = document.querySelector('.playground');
 let playgroundRect = playground.getBoundingClientRect();
-const IMG_SIZE = 80;
+let timer = document.querySelector('.play-notice__timer');
+let counter = document.querySelector('.play-notice__count');
+let icon = document.querySelector('.icon');
 
 function init() {
     // 1. 게임을 시작한다
-    // 1.1 당근과 벌레를 게임 필드안에 배치
-        // 1.2 타이머 작동 
+    // 1.1 당근과 벌레를 게임 필드안에 배치 o
+        // 1.2 타이머 작동 0
     // 2. 당근을 잡는다
-        // 2.1 카운트를 줄인다
+        // 2.1 카운트를 줄인다 0
     // 3. 게임을 종료 및 팝업 실행한다
         // 3.1 당근을 다 잡으면, win 종료 팝업 실행
         // 3.2 타이머 종료와 벌레를 잡으면, lost 종료 팝업 실행
     // 4. 리플레이 버튼을 누른다
         // 4.1 리플레이할 것인지 팝업 실행
 
+    timer.innerHTML = "0:0";
+    counter.innerHTML = IMG_COUNT;
     // 당근 추가
-    addItems('carrot', 5, 'img/carrot.png');
+    addItems('carrot', IMG_COUNT, 'img/carrot.png');
     // 벌레 추가
-    addItems('bug', 5, 'img/bug.png');
-
+    addItems('bug', IMG_COUNT, 'img/bug.png');
+    
+    // setInterval(() => {setTimer();}, 1000);
 }
 
 function addItems(item, cnt, img) {
@@ -44,5 +52,32 @@ function addItems(item, cnt, img) {
     }
 }
 
+playground.addEventListener('click', (e) => {
+    let class_name = e.target.className;
+
+    if(class_name == "playground"){
+        return;
+    }
+
+    if(class_name.includes('carrot')){
+        e.target.remove();
+        getCounter(IMG_COUNT);
+    }else if(class_name.indexOf('bug')){
+
+    }
+});
+
+function getCounter(cnt) {
+    cnt--;
+    counter.innerHTML = cnt;
+    IMG_COUNT = cnt;
+}
+
+function setTimer() {
+    if(GAME_TIME != 0){
+        timer.innerHTML = `0:${GAME_TIME}`;
+        GAME_TIME--;
+    }
+}
 
 init();
