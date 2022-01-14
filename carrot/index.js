0eed8b179db9d8ba3eccaf648f657f4d3ba81410
chinyday <47979888+chinyday@users.noreply.github.com>
2022-01-15 00:56:53 +0900

    게임 버튼, 타이머 기능, 팝업 영역 추가

--- carrot/index.js
+++ carrot/index.js
@@ -2,37 +2,34 @@
 
 const IMG_SIZE = 80;
 let IMG_COUNT = 5;
-let GAME_TIME = 20
+let GAME_TIME = 5;
 let playground = document.querySelector('.playground');
-let playgroundRect = playground.getBoundingClientRect();
 let timer = document.querySelector('.play-notice__timer');
 let counter = document.querySelector('.play-notice__count');
-let icon = document.querySelector('.icon');
+let play_btn = document.querySelector('.play-notice__button');
+let alert = document.querySelector('.alert');
+let alert_txt = document.querySelector('.alert__text');
+let alert_btn = document.querySelector('.alert__button');
+let interval = null;
 
 function init() {
-    // 1. 게임을 시작한다
-    // 1.1 당근과 벌레를 게임 필드안에 배치 o
-        // 1.2 타이머 작동 0
-    // 2. 당근을 잡는다
-        // 2.1 카운트를 줄인다 0
-    // 3. 게임을 종료 및 팝업 실행한다
-        // 3.1 당근을 다 잡으면, win 종료 팝업 실행
-        // 3.2 타이머 종료와 벌레를 잡으면, lost 종료 팝업 실행
-    // 4. 리플레이 버튼을 누른다
-        // 4.1 리플레이할 것인지 팝업 실행
 
+    IMG_COUNT = 5;
+    GAME_TIME = 5;
     timer.innerHTML = "0:0";
     counter.innerHTML = IMG_COUNT;
-    // 당근 추가
-    addItems('carrot', IMG_COUNT, 'img/carrot.png');
-    // 벌레 추가
-    addItems('bug', IMG_COUNT, 'img/bug.png');
+    playground.innerHTML = '';
+    alert.classList.add('alert--none');  
+
+    addItems('carrot', IMG_COUNT, 'img/carrot.png');// 당근 추가
+    addItems('bug', IMG_COUNT, 'img/bug.png');// 벌레 추가
     
-    // setInterval(() => {setTimer();}, 1000);
+    interval = setInterval(() => {setTimer();}, 1000);
 }
 
 function addItems(item, cnt, img) {
-  
+   
+    let playgroundRect = playground.getBoundingClientRect();
     let x1 = 0;
     let y1 = 0;
     let x2 = playgroundRect.width - IMG_SIZE;
@@ -62,8 +59,9 @@ playground.addEventListener('click', (e) => {
     if(class_name.includes('carrot')){
         e.target.remove();
         getCounter(IMG_COUNT);
-    }else if(class_name.indexOf('bug')){
-
+    }else if(class_name.includes('bug')){
+        clearInterval(interval);
+        displayPopUp('YOU LOST!!');
     }
 });
 
@@ -71,13 +69,47 @@ function getCounter(cnt) {
     cnt--;
     counter.innerHTML = cnt;
     IMG_COUNT = cnt;
-}
-
-function setTimer() {
-    if(GAME_TIME != 0){
-        timer.innerHTML = `0:${GAME_TIME}`;
-        GAME_TIME--;
+    
+    if(IMG_COUNT === 0 ){
+        clearInterval(interval);
+        displayPopUp('YOU WIN!!');
     }
 }
 
-init();
+function displayPopUp(txt) {
+    alert.classList.remove('alert--none');
+    alert_txt.innerHTML = txt;
+}
+
+function setTimer() {
+
+   if(GAME_TIME > 0){
+        GAME_TIME--;
+        timer.innerHTML = `0:${GAME_TIME}`;
+    }else if(GAME_TIME == 0){
+        displayPopUp('YOU LOST!!');
+    } 
+  
+}
+
+alert_btn.addEventListener('click', (e) => {
+    // let stop = play_btn.querySelector('.fa-stop');
+    // stop.classList.add('fa-play');
+    // stop.classList.remove('fa-stop');
+    clearInterval(interval);
+    init();
+}) 
+
+play_btn.addEventListener('click', (e) => { 
+
+    let play = play_btn.querySelector('.fa-play'); 
+
+    if(play){
+        play.classList.remove('fa-play');
+        play.classList.add('fa-stop');
+        init();
+    }else if(stop){
+        displayPopUp('REPLAY??');
+    }
+
+});
